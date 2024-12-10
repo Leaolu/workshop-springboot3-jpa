97bf49e8bb7414655f2e28a4628df528668a9287
Lucas Leao <lucasleao@gmail.com>
2024-12-10 12:05:00 -0300

    Service layer, component registration

--- src/main/java/com/EACH/demo/repositories/UserRepository.java
+++ src/main/java/com/EACH/demo/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.EACH.demo.entities.User;
 
+//annotation @Repository isn't mandatory
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
