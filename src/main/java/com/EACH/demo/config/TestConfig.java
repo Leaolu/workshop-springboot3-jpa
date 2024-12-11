87b80494ee1fd5256b6b1b1af0e43ec881d77934
Lucas Leao <lucasleao@gmail.com>
2024-12-11 12:25:32 -0300

    Payment, one-to-one association

--- src/main/java/com/EACH/demo/config/TestConfig.java
+++ src/main/java/com/EACH/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.EACH.demo.entities.Category;
 import com.EACH.demo.entities.Order;
 import com.EACH.demo.entities.OrderItem;
+import com.EACH.demo.entities.Payment;
 import com.EACH.demo.entities.Product;
 import com.EACH.demo.entities.User;
 import com.EACH.demo.entities.enums.OrderStatus;
@@ -83,6 +84,10 @@ public class TestConfig implements CommandLineRunner{
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 	
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 	
