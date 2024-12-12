dbc0c260384621e0488294c3e6d3ff20eb4547d0
Lucas Leao <lucasleao@gmail.com>
2024-12-12 11:07:53 -0300

    OrderService and OrderResource: insert and delete functions with exception treatment

--- src/main/java/com/EACH/demo/services/OrderServices.java
+++ src/main/java/com/EACH/demo/services/OrderServices.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.EACH.demo.entities.Order;
 import com.EACH.demo.repositories.OrderRepository;
+import com.EACH.demo.services.exceptions.DatabaseException;
+import com.EACH.demo.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class OrderServices {
@@ -21,6 +26,20 @@ public class OrderServices {
 	
 	public Order findById(Long id) {
 		Optional<Order> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public Order insert(Order obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 }
