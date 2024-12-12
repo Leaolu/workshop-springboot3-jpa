a045386aa8cd66ab1d42e1a62d8c7cab69d57916
Lucas Leao <lucasleao@gmail.com>
2024-12-12 12:01:11 -0300

    Category: insert, delete, update with exception handling

--- src/main/java/com/EACH/demo/services/CategoryServices.java
+++ src/main/java/com/EACH/demo/services/CategoryServices.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.EACH.demo.entities.Category;
 import com.EACH.demo.repositories.CategoryRepository;
+import com.EACH.demo.services.exceptions.DatabaseException;
+import com.EACH.demo.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CategoryServices {
@@ -21,6 +26,30 @@ public class CategoryServices {
 	
 	public Category findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	public Category update(Long id, Category obj) {
+		Category entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+	
+	private void updateData(Category entity, Category obj) {
+		entity.setName(obj.getName());
 	}
 }
