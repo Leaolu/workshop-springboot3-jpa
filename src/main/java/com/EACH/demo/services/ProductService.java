6996b8607100711168682f7ce6cd28f140f364ed
Lucas Leao <lucasleao@gmail.com>
2024-12-12 11:34:43 -0300

    Product: insert, delete, update with exception handling

--- src/main/java/com/EACH/demo/services/ProductService.java
+++ src/main/java/com/EACH/demo/services/ProductService.java
@@ -4,23 +4,60 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.EACH.demo.entities.Product;
 import com.EACH.demo.repositories.ProductRepository;
+import com.EACH.demo.services.exceptions.DatabaseException;
+import com.EACH.demo.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ProductService {
-	
+
 	@Autowired
 	private ProductRepository repository;
-	
-	public List<Product> findAll(){
+
+	public List<Product> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Product findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+
+	public Product insert(Product obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	public Product update(Long id, Product obj) {
+		try {
+		Product entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Product entity, Product obj) {
+		entity.setDescription(obj.getDescription());
+		entity.setImgUrl(obj.getImgUrl());
+		entity.setName(obj.getName());
+		entity.setPrice(obj.getPrice());
+
 	}
 }
